8d03cd2814b52f08ff7d00a2e4c43e30220e206d
ssavitski <ssavitski@thumbtack.net>
2016-04-20 15:49:35 +0300

    Refactoring sass files

--- app/js/main.js
+++ app/js/main.js
@@ -554,12 +554,11 @@ var app;
             .service('dataAccessService', DataAccessService);
     })(common = app.common || (app.common = {}));
 })(app || (app = {}));
+/// <reference path="angular-file-upload/angular-file-upload.d.ts" />
+/// <reference path="angular-scroll/angular-scroll.d.ts" />
+/// <reference path="angularjs/angular-animate.d.ts" />
+/// <reference path="angularjs/angular-resource.d.ts" />
+/// <reference path="angularjs/angular-route.d.ts" />
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
-/// <reference path="angularjs/angular-animate.d.ts" />
-/// <reference path="angular-scroll/angular-scroll.d.ts" />
-/// <reference path="angularjs/angular-route.d.ts" />
-/// <reference path="angularjs/angular-resource.d.ts" />
-/// <reference path="angular-file-upload/angular-file-upload.d.ts" />
 /// <reference path="ng-file-upload/ng-file-upload.d.ts" />
-/// <reference path="../node_modules/immutable/dist/immutable.d.ts" />
