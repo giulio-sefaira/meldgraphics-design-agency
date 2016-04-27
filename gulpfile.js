fee320c8d0ab32bb967c12305f2b04376243bb6b
ssavitski <ssavitski@thumbtack.net>
2016-04-27 18:20:29 +0300

    Add npm scripts & change animation effect of some elements

--- gulpfile.js
+++ gulpfile.js
@@ -18,11 +18,17 @@ var gulp				  = require('gulp'),
     changed 			= require('gulp-changed'),
     Config        = require('./gulpfile.config'),
     tsProject     = ts.createProject('tsconfig.json'),
-    order         = require("gulp-order");
+    order         = require("gulp-order"),
+    clean         = require('gulp-clean');
 
 var config = new Config();
 
-gulp.task('scripts', function() {
+gulp.task('clean', function () {
+  return gulp.src(config.sourceApp, {read: false})
+    .pipe(clean());
+});
+
+gulp.task('dev:scripts', function() {
 	return gulp.src([config.typescript,
                    config.libraryTypeScriptDefinitions])
     .pipe(order([
@@ -32,40 +38,71 @@ gulp.task('scripts', function() {
     ]))
     .pipe(concat('main.ts'))
     .pipe(ts(tsProject))
-    // .pipe(uglify())
-    // .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest(config.tsOutputPath))
     .pipe(browserSync.stream());
 });
 
-gulp.task('styles', function() {
+gulp.task('prod:scripts', function() {
+  return gulp.src([config.typescript,
+                   config.libraryTypeScriptDefinitions])
+    .pipe(order([
+      'app/app.ts',
+      'app/controllers/meldgraphicsCtrl.ts',
+      '**/*.ts'
+    ]))
+    .pipe(concat('main.ts'))
+    .pipe(ts(tsProject))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(config.tsOutputPath));
+});
+
+gulp.task('dev:styles', function() {
   return gulp.src(config.scss)
   	.pipe(sass({
-  		// outputStyle: 'compressed'
   		 includePaths: [
         './bower_components/compass-breakpoint/stylesheets'
   		 ]
   	}))
-    /*.pipe(autoprefixer({
-      browsers: ['last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
-      cascade: false
-    }))*/
-    // pipe(cssmin())
-		// .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest(config.scssOutputPath))
     .pipe(browserSync.stream());
 });
 
-gulp.task('html', function() {
+gulp.task('prod:styles', function() {
+  return gulp.src(config.scss)
+    .pipe(sass({
+       includePaths: [
+        './bower_components/compass-breakpoint/stylesheets'
+       ]
+    }))
+    .pipe(autoprefixer({
+      browsers: ['last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'],
+      cascade: false
+    }))
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest(config.scssOutputPath));
+});
+
+gulp.task('dev:html', function() {
 	return gulp.src(config.html)
 		.pipe(wiredep({
 	      directory: './bower_components'
 	    }))
 		.pipe(useref())
-	 //  .pipe(gulpif('*.js', uglify()))
-	 //  .pipe(gulpif('*.css', cssmin()))
 		.pipe(gulp.dest(config.sourceApp))
 		.pipe(browserSync.stream());
+});
+
+gulp.task('prod:html', function() {
+  return gulp.src(config.html)
+    .pipe(wiredep({
+        directory: './bower_components'
+      }))
+    .pipe(useref())
+    .pipe(gulpif('*.js', uglify()))
+    .pipe(gulpif('*.css', cssmin()))
+    .pipe(gulp.dest(config.sourceApp));
 });
 
 // Optimization of images
@@ -80,7 +117,7 @@ gulp.task('images', function () {
       .pipe(gulp.dest(config.imageOutputPath));
 });
 
-gulp.task('copy-images', function () {
+gulp.task('dev:images', function () {
   return gulp.src(config.images)
       .pipe(changed(config.imageOutputPath))
       .pipe(gulp.dest(config.imageOutputPath));
@@ -99,26 +136,30 @@ gulp.task('base64', ['images'], function () {
       .pipe(gulp.dest(config.scssOutputPath));
 });
 
+// Server running
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./app"
+        }
+    });
+});
+
 
 /**
- * Watch task for coffee, scss files
+ * Watch task for typescript, scss files
  *
  */
 gulp.task('watch', function() {
-	gulp.watch(config.typescript, ['scripts'], browserSync.reload);
-	gulp.watch(config.scss, ['styles'], browserSync.reload);
-	gulp.watch(config.html, ['html'], browserSync.reload);
-	gulp.watch(config.images, ['copy-images'], browserSync.reload);
+	gulp.watch(config.typescript, ['dev:scripts'], browserSync.reload);
+	gulp.watch(config.scss, ['dev:styles'], browserSync.reload);
+	gulp.watch(config.html, ['dev:html'], browserSync.reload);
+	gulp.watch(config.images, ['dev:images'], browserSync.reload);
 });
+
 
 /**
  * Default task
  *
  */
-gulp.task('default', ['scripts', 'styles', 'html', 'base64', 'watch'], function() {
-
-  browserSync.init({
-    server: './app'
-  });
-
-});
+gulp.task('default', ['clean', 'dev:scripts', 'dev:styles', 'dev:html', 'base64', 'watch', 'browser-sync']);
